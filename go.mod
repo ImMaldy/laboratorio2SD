bc4633bba527aabf966c89b03f66a8db11ef9e8f
Ignacio Maldonado <ignacio.maldonado@usm.cl>
2023-10-18 18:46:25 -0300

    xd

--- go.mod
+++ go.mod
@@ -1,14 +1,8 @@
-module containerized-go-app
+module github.com/tomiok/grpc-test-wishlist
 
-go 1.18
+go 1.13
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/streadway/amqp v1.1.0 // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
-	google.golang.org/grpc v1.59.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	github.com/golang/protobuf v1.4.3
+	google.golang.org/grpc v1.33.1
 )
--- servidor/main.go
+++ servidor/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-	pb "https://github.com/ImMaldy/laboratorio2SD/tree/4ab5257af5d27c40df8923a5e516cd35b314c830/proto"
+	"context"
+	"net"
+
+	pb "github.com/ImMaldy/laboratorio2SD/tree/4ab5257af5d27c40df8923a5e516cd35b314c830/proto"
+	"google.golang.org/grpc"
 )
 
 type server struct {
-	pb.Unim
+	pb.UnimplementedEnviarServer
+}
+
+func (s *server) Enviar_Continente(ctx context.Context, req *pb.Continente) (*pb.ResponderTodo, error) {
+	return nil, nil
+}
+
+func (s *server) RegistrarEn_Datanode(ctx context.Context, req *pb.RegistroDatanode) (*pb.ResponderTodo, error) {
+	return nil, nil
+}
+
+func (s *server) ConsultarPor_Id(ctx context.Context, req *pb.ConsultaId) (*pb.RespuestaId, error) {
+	return nil, nil
+}
+
+func (s *server) ConsultarPor_Estado(ctx context.Context, req *pb.ConsultaEstado) (*pb.RespuestaEstado, error) {
+	return nil, nil
+}
+
+func main() {
+	listner, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic("cannot create tcp connection" + err.Error())
+	}
+
+	serv := grpc.NewServer()
+	pb.RegisterEnviarServer(serv, &server{})
+
+	if err = serv.Serve(listner); err != nil {
+		panic("cannot initialize the server" + err.Error())
+	}
+
 }
